c5d63cb88f18909206de7fa4aac886bf164cacde
Soonpil Kang <spkang1123@gmail.com>
2026-08-15 10:45:05 -0700

    bug fix qt.

--- Src/EB/AMReX_EB_StateRedistribute.cpp
+++ Src/EB/AMReX_EB_StateRedistribute.cpp
@@ -154,11 +154,11 @@ MLStateRedistribute ( Box const& bx, int ncomp,
                             qt(i,j,k,i_nbor) = fac * U_in(r,s,t,n) * vfrac(r,s,t) / nbhd_vol(i,j,k);
                             Qhat(i,j,k,n) += qt(i,j,k,i_nbor);
                         }
-                        else
-                        {
-                            // Neighbor outside domain_per_grown contributes zero
-                            qt(i,j,k,i_nbor) = 0.0;
-                        }
+                        // else
+                        // {
+                        //     // Neighbor outside domain_per_grown contributes zero
+                        //     qt(i,j,k,i_nbor) = 0.0;
+                        // }
                     }
                 }
             } else {
@@ -309,6 +309,10 @@ MLStateRedistribute ( Box const& bx, int ncomp,
                             // Scale the slopes for each qt
                             //
                             Real q_over_Q = fac2*vfrac(ii,jj,kk)/nbhd_vol(i,j,k);
+
+                            // Skip if neighbor (ii,jj,kk) is outside domain_per_grown
+                            // This matches the write condition in the first loop (line 152)
+                            if (!domain_per_grown.contains(IntVect(AMREX_D_DECL(ii,jj,kk)))) continue;
 
                             Real update = qt(i,j,k,r_nbor);
                             AMREX_D_TERM(update += q_over_Q * lim_slope[0] *
